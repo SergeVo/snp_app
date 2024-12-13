b0ae17bd3c7708b4645152b01ff11a0307e169bf
Сергей Догадин <me@sdogadin.ru>
2024-12-13 13:08:35 +0300

    added requirements

--- snp/polls/views.py
+++ snp/polls/views.py
@@ -12,8 +12,9 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
@@ -34,10 +35,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return render(
-            request,
-            "polls/detail.html",
-            {
+        return render(request, "polls/detail.html", {
                 "question": question,
                 "error_message": "Не выбран вариант ответа!",
             },
--- snp/snp/urls.py
+++ snp/snp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls, namespace='debug_toolbar')),
+        path('__debug__/', include(debug_toolbar.urls,
+                                   namespace='debug_toolbar')),
     ]
