c468b53fa3dcd19d98cb74962f1c23ed87955dd8
Сергей Догадин <me@sdogadin.ru>
2024-12-16 16:46:41 +0300

    added bootstrap

--- snp/snp/settings.py
+++ snp/snp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-ooph20iv7hekuu9z=3a5(p=u9+oswt1m*!6mhv!zo=d@uf^&g^"
+# SECRET_KEY = "ключ_спрятан_в_файле_dotenv"
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
